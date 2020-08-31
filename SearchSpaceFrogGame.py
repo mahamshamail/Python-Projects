26239bdd307d7dfd1bb1bc9e42b2e6b1014762cd
Saiyeda Maham Shamail <50436238+mahamshamail@users.noreply.github.com>
2020-08-31 18:29:52 -0400

    Update SearchSpaceFrogGame.py

--- SearchSpaceFrogGame.py
+++ SearchSpaceFrogGame.py
@@ -1,42 +1,85 @@
-def mov(b):
-    m = []
-    for p, piece in enumerate( b ):
-        j = p + (piece * 2)
-        m2 = p + (piece)
-        if ( piece == 0 ):
-            continue
-        if (not (( j < 0 ) or ( j >= len(b)))):
-            if (b[j] == 0):
-                t = list(b)
-                t[p] = 0
-                t[j] = piece
-                m.append(t)
-        if (not ((m2 < 0) or ( m2 >= len(b)))):
-            if ( b[m2] == 0):
-                t = list(b)
-                t[p] = 0
-                t[m2] = piece
-                m.append(t)
-    return m
+s = [1,1,1,0,2,2,2]
 
-def evaluate( c, tar ):
-    next = []
-    for a in c:
-        n = mov(a[-1])
-        for q in n:
-            t = list(a)
-            t.append(q)
-            if ( q == tar ):
-                return t
-            next.append(t)
-    return next
+def validsteps (totalsteps):
+  steps = []
+  for index, frog in enumerate( totalsteps ):
+    j=0
+    m=index
+    if(frog==1):
+       j=index+2
+       m=m+1
+    elif (frog==2):
+      j=index-2
+      m=m-1
+    else:
+      j=0
+        
+    if ( frog == 0 ):
+      continue
+    if (not (( j < 0 ) or ( j >= len(totalsteps)))):
+      if (totalsteps[j] == 0):
+        t = list(totalsteps)
+        t[index] = 0
+        t[j] = frog
+        steps.append(t)
+    if (not ((m < 0) or ( m >= len(totalsteps)))):
+      if ( totalsteps[m] == 0):
+        t = list(totalsteps)
+        t[index] = 0
+        t[m] = frog
+        steps.append(t)
+  return steps
 
-def arr(start):
+
+def solAll( current, target ):
+  next = []
+  k=-1
+  a2=0
+  for a in current:
+    
+    k=k+1
+    n = validsteps(a[-1])
+    #print("\t")
+    if(len(a)-1==0):
+      print("~~~~~~~~~~~~~~~~~ ROOT NODE ~~~~~~~~~~~~~~~~~~~~~~~  ")
+    elif( a2 != len(a)-1):
+      print("\n##################### Depth "+str(len(a)-1) +" ##################### ")
+      a2=len(a)-1
+    else:
+      print("\n____________________________________________________")
+    print("\n"+str(a[len(a)-1])+" at DEPTH: "+str(len(a)-1) +" NODE: "+str(k)+"\nHas the following children:")
+    print(n)
+
+    for q in n:
+      #print(" jhh   ")
+      t = list(a)
+      t.append(q)
+      if ( q == target ):
+        #print("sdsd")
+        return t
+      next.append(t)
+    
+  return next
+
+
+
+
+def win(start):
+    #print(start)
     temp=[[start]]
+    #print (temp)
+    #print("her")
+    #print(temp)
     end = list(start)
     end.reverse()
+    #print(end)
+    #print("uuu")
     while(temp[-1] != end):
-        temp = evaluate(temp, end)
+        #print(temp[-1])
+        #print("uuu77")
+        temp = solAll(temp, end)
+        #print(temp)
+        #print("\n-----------------------------------------------------")
     return temp
-a= [1, 1, 1, 0, -1, -1, -1]
-print(arr(a))
+
+win(s)
